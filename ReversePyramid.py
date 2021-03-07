253eef5783809395fb659f91ede190edc1c9e93b
vaibhavdixit07 <vaibhavdixit1000@gmail.com>
2021-03-07 19:52:09 +0530

    ReversePyramid.py commit

---
+++ ReversePyramid.py
@@ -0,0 +1,24 @@
+class ReversePyramid:
+    def __init__(self, rows):
+        self.rows = rows
+
+    def printPyramid(self):
+        for i in range(1, self.rows + 1):
+            for j in range(1, i):
+                print(" ", end="")
+            for j in range(self.rows - i, -1, -1):
+                print("*", end="")
+            for j in range(self.rows - i, 0, -1):
+                print("*", end="")
+            print()
+
+
+class ChildReversePyramid(ReversePyramid):
+    def __init__(self, r):
+        super().__init__(r)
+
+
+if __name__ == '__main__':
+    row = int(input("Enter the number of rows: "))
+    obj = ChildReversePyramid(row)
+    obj.printPyramid()
